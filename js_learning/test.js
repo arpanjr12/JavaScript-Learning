3c6606374e36072a0290d2175f2668bd166deaac
Arpan Das <arpandasjr03@gmail.com>
2024-09-13 03:38:49 +0000

    container config done

---
+++ js_learning/test.js
@@ -0,0 +1,4 @@
+console.log("Arpan")
+
+/*JavaScript is a dynamic language and not static .which meance that variables can hold values of different type s during run time...
+unlike language such as TypeScript or java,you don't need to declear the data type of a variable explicitly*/
